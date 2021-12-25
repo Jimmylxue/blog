d08657b722d5ed23c287bbaed1d5b565c8a1c086
jimmyxuexue <1002661758@qq.com>
2021-12-25 13:55:41 +0800

    新增sh操作

---
+++ start.sh
@@ -0,0 +1,8 @@
+# 删除容器
+docker rm -f vitepressBlog &> /dev/null
+
+# 重启容器
+docker run -d --restart=on-failure:5\
+    -p 999:80 \
+    -v $PWD/public:/usr/share/nginx/html \
+    --name vitepressBlog nginx
