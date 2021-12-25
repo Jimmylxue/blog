1f26fe611130960eb23e827e23c887795a263986
jimmyxuexue <1002661758@qq.com>
2021-12-25 11:53:20 +0800

    更改

--- docs/.vitepress/Jimmy-theme/index.js
+++ docs/.vitepress/Jimmy-theme/index.js
@@ -1,7 +1,7 @@
 // import './styles/index.scss';
 // import '@devui-design/icons/icomoon/devui-icon.css';
-import Layout from "./Layout.vue";
-import NotFound from "./NotFound.vue";
+import Layout from './layout.vue';
+import NotFound from './NotFound.vue';
 const theme = {
   Layout,
   NotFound,
