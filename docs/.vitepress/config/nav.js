a3bf0d63b1cdb56d453e0fad23b3f437b6679725
jimmyxuexue <1002661758@qq.com>
2022-05-22 10:52:12 +0800

    诺基亚更新

--- docs/.vitepress/config/nav.js
+++ docs/.vitepress/config/nav.js
@@ -1,42 +1,45 @@
 const nav = [
-  { text: '首页', link: '/' },
-  {
-    text: '小工具',
-    icon: 'reco-message',
-    items: [
-      {
-        text: '学习时钟',
-        // link: '/article/介绍/自我介绍',
-        link: 'http://www.jimmyxuexue.top:667/study/',
-      },
-    ],
-  },
-  {
-    text: '其他网站',
-    icon: 'reco-message',
-    items: [
-      {
-        text: 'Gitee',
-        link: 'https://gitee.com/jimmyxuexue',
-        icon: 'reco-github',
-      },
-      {
-        text: 'bilibili',
-        link: 'https://space.bilibili.com/304985153',
-        icon: 'reco-github',
-      },
-      {
-        text: '个人空间',
-        link: 'http://www.jimmyxuexue.top/',
-        icon: 'reco-github',
-      },
-      {
-        text: '稀土掘金',
-        link: 'https://juejin.cn/user/2296218359183918',
-        icon: 'reco-github',
-      },
-    ],
-  },
-];
+	{ text: '首页', link: '/' },
+	{
+		text: '小工具',
+		icon: 'reco-message',
+		items: [
+			{
+				text: '诺基亚表情包生成器',
+				link: 'http://www.jimmyxuexue.top:667/Nokia/',
+			},
+			{
+				text: '学习时钟',
+				link: 'http://www.jimmyxuexue.top:667/study/',
+			},
+		],
+	},
+	{
+		text: '其他网站',
+		icon: 'reco-message',
+		items: [
+			{
+				text: 'github',
+				link: 'https://github.com/Jimmylxue',
+				icon: 'reco-github',
+			},
+			{
+				text: 'bilibili',
+				link: 'https://space.bilibili.com/304985153',
+				icon: 'reco-github',
+			},
+			{
+				text: '个人空间',
+				link: 'http://www.jimmyxuexue.top/',
+				icon: 'reco-github',
+			},
+			{
+				text: '稀土掘金',
+				link: 'https://juejin.cn/user/2296218359183918',
+				icon: 'reco-github',
+			},
+		],
+	},
+]
 
-export default nav;
+export default nav
