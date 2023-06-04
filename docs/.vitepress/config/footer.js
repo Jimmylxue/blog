1c28b7cb52f32bf15da7d585da0581db7fc54859
jimmylxuexue <1002661758@qq.com>
2023-06-04 16:28:22 +0800

    🎉 接入又拍云

--- docs/.vitepress/config/footer.js
+++ docs/.vitepress/config/footer.js
@@ -1,4 +1,5 @@
 export const footer = {
-	message: '© jimmy-知识星球',
+	message:
+		'<p style="display: flex;justify-content: center;align-items: center;font-weight: 600;">本站由<img width="40" height="40" src="http://image.jimmyxuexue.top/img/202306041622530.png" alt="">提供CDN加速/云存储服务</p>',
 	copyright: '闽ICP备2021016313号-1',
 }
