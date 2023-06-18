74abb98e8f5e1ca4c9dc5543e793c2ca777d1e51
jimmylxuexue <1002661758@qq.com>
2023-06-18 20:24:53 +0800

    🙈 add ignorw

--- docs/.vitepress/config/footer.js
+++ docs/.vitepress/config/footer.js
@@ -1,5 +1,5 @@
 export const footer = {
 	message:
-		'<p style="display: flex;justify-content: center;align-items: center;font-weight: 600;">本网站由<img width="40" height="40" src="http://image.jimmyxuexue.top/img/202306041622530.png" alt="">提供CDN加速/云存储服务</p>',
+		'<a href="https://www.upyun.com/?utm_source=lianmeng&utm_medium=referral" style="display: flex;justify-content: center;align-items: center;font-weight: 600;">本网站由<img width="40" height="40" src="http://image.jimmyxuexue.top/img/202306041622530.png" alt="">提供CDN加速/云存储服务</a>',
 	copyright: '闽ICP备2021016313号-1',
 }
